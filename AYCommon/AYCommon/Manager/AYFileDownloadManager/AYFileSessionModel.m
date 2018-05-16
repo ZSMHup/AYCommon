52fb385cae213d8743bc5e28e8f7de24c123d02f
张书孟 <zhang_shu_meng@163.com>
2018-05-16 15:50:15 +0800

    [commit]: 添加文件下载，webView，二维码相关

--- AYCommon/AYCommon/Manager/AYFileDownloadManager/AYFileSessionModel.m
+++ AYCommon/AYCommon/Manager/AYFileDownloadManager/AYFileSessionModel.m
@@ -1,13 +1,13 @@
 //
-//  AYCommon.h
+//  AYFileSessionModel.m
 //  AYCommon
 //
 //  Created by 张书孟 on 2018/5/16.
 //  Copyright © 2018年 ZSM. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import "AYFileSessionModel.h"
 
-@interface AYCommon : NSObject
+@implementation AYFileSessionModel
 
 @end
