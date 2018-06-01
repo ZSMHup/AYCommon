0b1980286c5fd058277289c92c8e2e84bdcb701c
张书孟 <zhang_shu_meng@163.com>
2018-06-01 17:59:50 +0800

    [commit]:

--- AYCommon/AYCommon/Macro/AYMatchUtils.h
+++ AYCommon/AYCommon/Macro/AYMatchUtils.h
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @interface AYMatchUtils : NSObject
 
@@ -17,5 +18,26 @@
  @return YES / NO
  */
 + (BOOL)checkPhoneNumInput:(NSString *)input;
+
+#pragma mark - 尺寸计算
+/**
+ 根据宽度求高度
+
+ @param text 计算的内容
+ @param width 最大宽度
+ @param font 字体大小
+ @return 高度
+ */
++ (CGFloat)getLabelHeightWithText:(NSString *)text width:(CGFloat)width font:(CGFloat)font;
+
+/**
+ 根据高度度求宽度
+
+ @param text 计算的内容
+ @param height 最大高度
+ @param font 字体大小
+ @return 宽度
+ */
++ (CGFloat)getWidthWithText:(NSString *)text height:(CGFloat)height font:(CGFloat)font;
 
 @end
